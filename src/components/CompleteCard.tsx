08889a625da43b335021b44b42a9b624dad9e492
Thangla <hongthang@v-takeuchi.vn>
2023-08-24 00:01:56 +0700

    Fix complete card

--- src/components/CompleteCard.tsx
+++ src/components/CompleteCard.tsx
@@ -1,5 +1,5 @@
-import { Card, CardHeader, Stack, Typography } from "@mui/material";
 import React, { ReactNode } from "react";
+import { Card, CardHeader, CardContent, CardActions, Stack, Typography } from "@mui/material";
 import LottieIcon from "./LottieIcon";
 import success from './json/success.json'
 import { isString } from "lodash";
@@ -8,7 +8,7 @@ import { RestProps } from "../utils/types";
 type Props = {
     cardTitle: string,
     title: string | ReactNode,
-    content: string | ReactNode,
+    content?: string | ReactNode,
     action?: ReactNode,
 }
 
@@ -18,18 +18,26 @@ const CompleteCard: React.FC<CompleteCardProps> = ({ cardTitle, title, content, 
     return (
         <Card {...props}>
             <CardHeader title={cardTitle} />
-            <Stack spacing={2}>
-                <LottieIcon animationData={success} />
-                <Stack spacing={1} alignItems='center'>
-                    {!isString(title) ? title : <Typography variant="subtitle1">
-                        {title}
-                    </Typography>}
-                    {!isString(content) ? content : <Typography variant="body2">
-                        {content}
-                    </Typography>}
+            <CardContent sx={{
+                p: 2,
+                borderRadius: 2,
+                border: theme => `1px dashed ${theme.palette.divider}`,
+            }}>
+                <Stack>
+                    <LottieIcon animationData={success} />
+                    <Stack spacing={1} alignItems='center'>
+                        {!isString(title) ? title : <Typography variant="subtitle1">
+                            {title}
+                        </Typography>}
+                        {!isString(content) ? content : <Typography variant="body2">
+                            {content}
+                        </Typography>}
+                    </Stack>
                 </Stack>
-                {action && action}
-            </Stack>
+            </CardContent>
+            {action && <CardActions>
+                {action}
+            </CardActions>}
         </Card>
     )
 }
