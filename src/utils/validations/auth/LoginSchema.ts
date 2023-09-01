74d55445fab8eb9287c46a108cfb163f6b0848a5
Thangla <hongthang@v-takeuchi.vn>
2023-09-01 17:13:41 +0700

    Fix Login schema

--- src/utils/validations/auth/LoginSchema.ts
+++ src/utils/validations/auth/LoginSchema.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup'
 
 export const LoginSchema = (t: any) => yup.object().shape({
-    username: yup.string().min(6, t('validations.username.min')).required(t('validations.username.required')),
-    password: yup.string().min(8, t('validations.password.min')).required(t('validations.password.required')),
+    username: yup.string().required(t('validations.username.required')).min(6, t('validations.username.min')),
+    password: yup.string().required(t('validations.password.required')).min(8, t('validations.password.min')),
 }).required()
