7baa63a744aa4b2c4ba7f020cc31402f649c53d0
Thangla <hongthang@v-takeuchi.vn>
2023-09-01 17:13:56 +0700

    Fix Security schema

--- src/utils/validations/personal-settings/SecuritySchema.ts
+++ src/utils/validations/personal-settings/SecuritySchema.ts
@@ -2,6 +2,6 @@ import * as yup from 'yup'
 
 export const SecuritySchema = (t: any) => yup.object().shape({
     oldPassword: yup.string().required(t('validations.oldPassword.required')),
-    newPassword: yup.string().min(8, t('validations.newPassword.min')).required(t('validations.newPassword.required')),
-    confirmPassword: yup.string().oneOf([yup.ref('newPassword')], t('validations.confirmPassword.match')).required(t('validations.confirmPassword.required')),
+    newPassword: yup.string().required(t('validations.newPassword.required')).min(8, t('validations.newPassword.min')),
+    confirmPassword: yup.string().required(t('validations.confirmPassword.required')).oneOf([yup.ref('newPassword')], t('validations.confirmPassword.match')),
 })
